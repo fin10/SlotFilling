b4c75d58da686fa0fe814b22e961ad74cc75a4d0
fin10 <fender64@gmail.com>
2017-04-07 18:53:11 +0900

    Add multilayer on rnn

--- simple_slot_filling.py
+++ simple_slot_filling.py
@@ -9,7 +9,8 @@ tf.logging.set_verbosity(tf.logging.INFO)
 EMBEDDING_DIMENSION = 128
 MAX_SENTENCE_LENGTH = 50
 CELL_SIZE = 128
-BATCH_SIZE = 64
+BATCH_SIZE = 300
+NUM_LAYERS = 3
 UNK = '<unk>'
 
 
@@ -77,75 +78,6 @@ def load_dataset(data_path: str, size: int = -1):
     }
 
 
-def rnn_model_fn(features, target, mode, params):
-    num_classes = params['num_classes']
-    learning_rate = params['learning_rate']
-    dropout = mode == tf.contrib.learn.ModeKeys.TRAIN and 0.5 or 1.0
-
-    cell = tf.contrib.rnn.GRUCell(CELL_SIZE)
-    cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=dropout)
-
-    weight = tf.Variable(tf.truncated_normal([CELL_SIZE, num_classes], stddev=0.01))
-    bias = tf.Variable(tf.constant(0.1, shape=[num_classes]))
-
-    inputs = features['labeled_inputs']
-    length = features['labeled_sequence_length']
-    mask = features['mask']
-
-    outputs, state = tf.nn.dynamic_rnn(
-        cell=cell,
-        inputs=inputs,
-        sequence_length=length,
-        dtype=tf.float32
-    )
-
-    output = tf.reshape(outputs, [-1, CELL_SIZE])
-    softmax = tf.nn.softmax(tf.matmul(output, weight) + bias)
-    prediction = tf.reshape(softmax, [-1, MAX_SENTENCE_LENGTH, num_classes])
-
-    loss = None
-    if mode != tf.contrib.learn.ModeKeys.INFER:
-        target = tf.one_hot(target, num_classes)
-        cross_entropy = -tf.reduce_sum(target * tf.log(prediction), reduction_indices=2) * mask
-        cross_entropy = tf.reduce_sum(cross_entropy, reduction_indices=1) / tf.cast(length, tf.float32)
-        loss = tf.reduce_mean(cross_entropy)
-
-    prediction = tf.argmax(prediction, 2)
-
-    train_op = None
-    if mode == tf.contrib.learn.ModeKeys.TRAIN:
-        train_op = tf.contrib.layers.optimize_loss(
-            loss=loss,
-            global_step=tf.train.get_global_step(),
-            learning_rate=learning_rate,
-            optimizer='RMSProp'
-        )
-
-    eval_metric_ops = None
-    if mode != tf.contrib.learn.ModeKeys.INFER:
-        target = tf.argmax(target, 2)
-        eval_metric_ops = {
-            'accuracy': tf.metrics.accuracy(
-                labels=target,
-                predictions=prediction,
-                weights=mask
-            ),
-            'predictions': prediction,
-            'target': target,
-            'length': length
-        }
-
-    return tf.contrib.learn.ModelFnOps(
-        mode=mode,
-        predictions={
-            'predictions': prediction,
-        },
-        loss=loss,
-        train_op=train_op,
-        eval_metric_ops=eval_metric_ops
-    )
-
-
 def input_fn(slot_vocab: dict, word_vocab: dict, dataset: dict, size: int = BATCH_SIZE):
     input_dict = {
         'labeled_inputs': [],
@@ -206,6 +138,78 @@ def input_fn(slot_vocab: dict, word_vocab: dict, dataset: dict, size: int = BATC
     return input_dict, labels
 
 
+def rnn_model_fn(features, target, mode, params):
+    num_classes = params['num_classes']
+    learning_rate = params['learning_rate']
+    dropout = mode == tf.contrib.learn.ModeKeys.TRAIN and 0.5 or 1.0
+
+    cell = tf.contrib.rnn.GRUCell(CELL_SIZE)
+    cell = tf.contrib.rnn.MultiRNNCell([cell] * NUM_LAYERS)
+    cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=dropout)
+
+    inputs = features['labeled_inputs']
+    length = features['labeled_sequence_length']
+    mask = features['mask']
+
+    outputs, state = tf.nn.dynamic_rnn(
+        cell=cell,
+        inputs=inputs,
+        sequence_length=length,
+        dtype=tf.float32
+    )
+
+    output = tf.reshape(outputs, [-1, CELL_SIZE])
+
+    with tf.name_scope('softmax'):
+        weight = tf.Variable(tf.truncated_normal([CELL_SIZE, num_classes], stddev=0.01), name='weights')
+        bias = tf.Variable(tf.constant(0.1, shape=[num_classes]), name='bias')
+        softmax = tf.nn.softmax(tf.matmul(output, weight) + bias)
+
+    prediction = tf.reshape(softmax, [-1, MAX_SENTENCE_LENGTH, num_classes])
+
+    loss = None
+    if mode != tf.contrib.learn.ModeKeys.INFER:
+        target = tf.one_hot(target, num_classes)
+        cross_entropy = -tf.reduce_sum(target * tf.log(prediction), reduction_indices=2) * mask
+        cross_entropy = tf.reduce_sum(cross_entropy, reduction_indices=1) / tf.cast(length, tf.float32)
+        loss = tf.reduce_mean(cross_entropy)
+
+    prediction = tf.argmax(prediction, 2)
+
+    train_op = None
+    if mode == tf.contrib.learn.ModeKeys.TRAIN:
+        train_op = tf.contrib.layers.optimize_loss(
+            loss=loss,
+            global_step=tf.train.get_global_step(),
+            learning_rate=learning_rate,
+            optimizer='RMSProp'
+        )
+
+    eval_metric_ops = None
+    if mode != tf.contrib.learn.ModeKeys.INFER:
+        target = tf.argmax(target, 2)
+        eval_metric_ops = {
+            'accuracy': tf.metrics.accuracy(
+                labels=target,
+                predictions=prediction,
+                weights=mask
+            ),
+            # 'predictions': prediction,
+            # 'target': target,
+            # 'length': length
+        }
+
+    return tf.contrib.learn.ModelFnOps(
+        mode=mode,
+        predictions={
+            'predictions': prediction
+        },
+        loss=loss,
+        train_op=train_op,
+        eval_metric_ops=eval_metric_ops
+    )
+
+
 def main(unused_argv):
     slot_vocab = load_slot_vocab('./atis.slot')
     word_vocab = load_word_vocab('./word2vec.embeddings')
@@ -224,14 +228,27 @@ def main(unused_argv):
             'num_classes': len(slot_vocab),
             'learning_rate': 0.01
         },
-        config=tf.contrib.learn.RunConfig(save_checkpoints_secs=300),
+        config=tf.contrib.learn.RunConfig(save_checkpoints_secs=5),
         model_dir='./model/'
     )
+
+    validation_metrics = {
+        "accuracy":
+            tf.contrib.learn.MetricSpec(
+                metric_fn=tf.contrib.metrics.streaming_accuracy,
+                prediction_key='predictions',
+                weight_key='mask'
+            )
+    }
 
     monitor = tf.contrib.learn.monitors.ValidationMonitor(
         input_fn=lambda: input_fn(slot_vocab, word_vocab, validation_set, validation_set['size']),
         eval_steps=1,
-        every_n_steps=10,
+        every_n_steps=50,
+        metrics=validation_metrics,
+        early_stopping_metric="loss",
+        early_stopping_metric_minimize=True,
+        early_stopping_rounds=300
     )
 
     classifier.fit(
@@ -245,21 +262,6 @@ def main(unused_argv):
         steps=1
     )["accuracy"]
     print('Accuracy: {0:f}'.format(accuracy_score))
-
-    result = classifier.evaluate(
-        input_fn=lambda: input_fn(slot_vocab, word_vocab, test_set, test_set['size']),
-        steps=1
-    )
-    print('Accuracy: {0:f}'.format(result["accuracy"]))
-
-    # total = 0
-    # count = 0
-    # for prediction, target, length in zip(result['predictions'].tolist(), result['target'].tolist(), result['length'].tolist()):
-    #     count += length
-    #     for p, t in zip(prediction[:length], target[:length]):
-    #         if p == t:
-    #             total += 1
-    # print('Accuracy: {0:f}({1}/{2})'.format(total/count, total, count))
 
 
 if __name__ == "__main__":
