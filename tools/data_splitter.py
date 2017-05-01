953510a92e632a4bc32b3a6f5d58a81c7ac42086
fin10 <fender64@gmail.com>
2017-05-02 00:35:09 +0900

    Update tools

--- tools/data_splitter.py
+++ tools/data_splitter.py
@@ -1,72 +1,19 @@
 import argparse
-import collections
-import re
-
-IOB_REGEX = re.compile('\(([^)]+)\)\[([^\]]+)\]')
+import random
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('slot')
     parser.add_argument('input')
-    parser.add_argument('ratio', type=float)
     args = parser.parse_args()
 
-    slots = dict()
-    with open(args.slot) as file:
-        for line in file:
-            line = line.strip()
-            if len(line) > 0:
-                slots[line] = 0
-
-    data = []
     with open(args.input) as file:
-        for line in file:
-            line = line.strip()
-            if len(line) > 0:
-                data.append(line)
-    print('# %s (%d)' % (args.input, len(data)))
+        data = [line.strip() for line in file if len(line.strip())]
 
-    target = []
-    for line in data:
-        for match in IOB_REGEX.finditer(line):
-            tokens = match.group(1).split(' ')
-            iob = ' '.join(['{}/{}-{}'.format(tokens[i], (i == 0 and 'b' or 'i'), match.group(2))
-                            for i in range(len(tokens))]).strip()
-            line = line.replace(match.group(0), iob)
+    size = len(data)
 
-        tags = []
-        tokens = line.strip().lower().split(' ')
-        for token in tokens:
-            if '/' in token:
-                tags.append(token.partition('/')[2])
-            else:
-                tags.append('o')
-        target.append(tags)
-
-    for tags in target:
-        for tag in tags:
-            slots[tag] += 1
-
-    for k, v in slots.items():
-        slots[k] = int(v * args.ratio)
-
-    slots = collections.OrderedDict(sorted(slots.items(), key=lambda x: x[0]))
-    with open(args.input + '_' + str(args.ratio) + '.csv', 'w') as file:
-        for k, v in slots.items():
-            file.write('{},{}\n'.format(k, v))
-
-    result = []
-    for sentence, tags in zip(data, target):
-        satisfied = True
-        for tag in tags:
-            slots[tag] -= 1
-            if slots[tag] < 0:
-                satisfied = False
-                break
-
-        if satisfied:
-            result.append(sentence)
-
-    print('# %d are generated.' % len(result))
-    with open(args.input + '_' + str(args.ratio), 'w') as file:
-        file.write('\n'.join(result))
+    for idx in range(1, 11):
+        random.shuffle(data)
+        subset = data[:int(size * 0.1 * idx)]
+        with open('{}_{}'.format(args.input, idx), mode='w') as output:
+            output.write('\n'.join(subset))
+        print('#%d (%d)' % (idx, len(subset)))
---
+++ tools/pkl_parser.py
@@ -0,0 +1,61 @@
+import argparse
+import pickle
+
+import random
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='Input a pkl file')
+    args = parser.parse_args()
+
+    with open(args.input, 'rb') as f:
+        train, test, dicts = pickle.load(f, encoding='latin1')
+
+
+    def invert(old: dict):
+        inverted = dict()
+        for item in old.items():
+            inverted[item[1]] = item[0].lower()
+        return inverted
+
+
+    idx2words = invert(dicts['words2idx'])
+    idx2labels = invert(dicts['labels2idx'])
+    idx2tables = invert(dicts['tables2idx'])
+
+    slots = set(idx2labels.values())
+    print('# Slots (%d)' % len(slots))
+
+
+    def parse(words_list, labels_list):
+        result = []
+        for words, labels in zip(words_list, labels_list):
+            tokens = []
+            for word, label in zip(words, labels):
+                tokens.append('{}/{}'.format(idx2words[word], idx2labels[label]))
+            result.append(' '.join(tokens))
+        return result
+
+
+    def divide(data: list, ratio: float):
+        random.shuffle(data)
+        pivot = int(len(data) * ratio)
+        return data[:pivot], data[pivot:]
+
+
+    train_data = parse(train[0], train[2])
+    train_data, dev_data = divide(train_data, 0.8)
+    test_data = parse(test[0], test[2])
+    print('# Train (%d), Dev (%d), Test (%d) are generated.' % (len(train_data), len(dev_data), len(test_data)))
+
+    with open(args.input + '.slot', 'w') as output:
+        output.write('\n'.join(slots))
+
+    with open(args.input + '.train', 'w') as output:
+        output.write('\n'.join(train_data))
+
+    with open(args.input + '.dev', 'w') as output:
+        output.write('\n'.join(dev_data))
+
+    with open(args.input + '.test', 'w') as output:
+        output.write('\n'.join(test_data))
